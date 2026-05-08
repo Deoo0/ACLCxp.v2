f46bf653ccf315546d031a8b8e8d636f6c3a574f
Phyl <acostacharles34@gmail.com>
2026-05-08 19:49:39 +0800

    Updated url call, fixed connectivity testing. Added 'api'on line 6.

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-  baseURL: `${API_URL}/users`,
+  baseURL: `${API_URL}/api/users`,
   headers: {
     'Content-Type': 'application/json',
   },
