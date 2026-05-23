65315d74b57ba51135d03f811d50b7d415c23a9e
Lawrence Deo Bersales <lawrencebersales8@gmail.com>
2026-05-23 13:57:51 +0800

    API endpoint for user list

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -5,4 +5,5 @@ urlpatterns=[
     path('health/', views.health_check, name='health_check'),
     path('echo/', views.echo_test, name='echo_test'),
     path('register/', views.register_user, name='register_user'),
+    path('list/', views.list_user, name='list-users'),
 ]
